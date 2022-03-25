927ab6e9db52d3d255230941e0ab03709fe54e10
Marco Medrano <marco.medrano.garay@gmail.com>
2022-03-25 01:32:41 -0400

    feat:sample working

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Graph;
+using Azure.Identity;
+
+var clientSecretCredential = new ClientSecretCredential(
+"<tenant_id>",
+ "<client_id>",
+  "<client_secret>");
+var graphClient = new GraphServiceClient(clientSecretCredential);
+var user = new User
+{
+    AccountEnabled = true,
+    DisplayName = "Marco Medrano",
+    MailNickname = "MarcoMedrano",
+    UserPrincipalName = "MarcoMedrano@MarcoMedranoGaraygmail.onmicrosoft.com",
+    PasswordProfile = new PasswordProfile
+    {
+        ForceChangePasswordNextSignIn = false,
+        Password = "!@#$!@#$"
+    }
+};
+
+try
+{
+    var userCreated = graphClient.Users
+        .Request()
+        .AddAsync(user).GetAwaiter().GetResult();
+    Console.WriteLine("By!" + userCreated.Id);
+}
+catch(System.Exception e)
+{
+    Console.WriteLine("Error " + e.Message);
+}
