2e8fb39dc903f19a3d83023cba65950213d55e6e
MarcoMedrano <marco.medrano.garay@gmail.com>
2022-07-18 14:48:37 -0400

    feat: list and delete users

--- Program.cs
+++ Program.cs
@@ -25,6 +25,17 @@ try
         .Request()
         .AddAsync(user).GetAwaiter().GetResult();
     Console.WriteLine("By!" + userCreated.Id);
+
+    // GET
+    // var u = await graphClient.Users["96bf5b08-ecdd-4692-83c0-89deb648c448"]
+	// .Request()
+	// .Select("userPrincipalName, displayName,givenName,postalCode,identities")
+	// .GetAsync();
+    // Console.WriteLine("User: " + u.UserPrincipalName);
+
+    // await graphClient.Users["96bf5b08-ecdd-4692-83c0-89deb648c448"]
+	// .Request()
+	// .DeleteAsync();
 }
 catch(System.Exception e)
 {
